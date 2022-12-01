2a32cd1c4207e5bea7cbd9ae02ca2d54c145c983
Cruella666 <116261952+Cruella666@users.noreply.github.com>
2022-12-01 11:41:14 +0100

    Add files via upload

---
+++ Program_FizzBuzz.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Please enter your first value: ");
+double value1 = double.Parse(Console.ReadLine()!);
+
+Console.Write("Please enter your second value: ");
+double value2 = double.Parse(Console.ReadLine()!);
+
+Console.Write("Please enter your result(rounded of two frictional digits): ");
+double result = double.Parse(Console.ReadLine()!);
+
+if (value1 + value2 == result) 
+{
+    Console.WriteLine("The operator is plus. ");
+}
+else if (value1 - value2 == result || value2 - value1 == result) 
+{
+    Console.WriteLine("The operator is minus. ");
+}
+else if (Math.Round((value1 / value2),2) == result || Math.Round((value2 / value1),2) == result) 
+{
+    Console.WriteLine("The operator is divided. ");
+}
+else if (value1 * value2 == result) 
+{
+    Console.WriteLine("The operator is multiply. ");
+}
+else if (value1 % value2 == result || value2 % value1 == result) 
+{
+    Console.WriteLine("The operator is modulo. ");
+}
+else if (Math.Pow(value1, value2) == result || Math.Pow(value2, value1) == result) 
+{
+    Console.WriteLine("The operator is exponentiation. ");
+}
+
